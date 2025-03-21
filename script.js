ed0225cbfca7c2cb63d7427a952174a32686e8e6
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-03-21 14:43:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const makeChange = (c) => {
   // your name here
 	let obj={ q:25,d:10,n:5,p:1 }
-	let ans={ q:0,d:0,n:0,p:0}}
+	let ans={ q:0,d:0,n:0,p:0}
 	for(let key in obj){
 		let temp=parseInt(c/obj[key])
 		ans[key]=temp
